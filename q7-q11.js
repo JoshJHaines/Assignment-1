a903cf315b88282b06a340710e8c58119c677df5
JoshuaJHaines <joshjhaines3@gmail.com>
2022-01-25 15:54:49 -0500

    Qs 7-9 done

--- q7-q11.js
+++ q7-q11.js
@@ -2,10 +2,22 @@ const x = 6
 
 // 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" using without using arrow functions.
 
+function add2nums(num1, num2){
+  return num1 + num2 + x
+}
+console.log("Question 7 Running: ", add2nums(1,2))
+
 // 8. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x", using arrow functions.
+const addNums = (num1, num2) => {
+  return num1 + num2 + x;
+}
+console.log("Question 8 Running: ", addNums(4, 5));
 
 // 9. Write a function that returns another function. (use arrow functions please)
-
+const returnFunction = () => {
+	return addNums;
+};
+console.log("Question 9 Running: ", returnFunction()(4, 5));
 
 // 10. given the following code:
 
