3d06d2f4803ea27bbde97e8130ad42b6e97738d2
JoshuaJHaines <joshjhaines3@gmail.com>
2022-01-25 16:44:09 -0500

    Qs done

--- q7-q11.js
+++ q7-q11.js
@@ -1,16 +1,16 @@
-const x = 6
+const x = 6;
 
 // 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" using without using arrow functions.
 
-function add2nums(num1, num2){
-  return num1 + num2 + x
+function add2nums(num1, num2) {
+	return num1 + num2 + x;
 }
-console.log("Question 7 Running: ", add2nums(1,2))
+console.log("Question 7 Running: ", add2nums(1, 2));
 
 // 8. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x", using arrow functions.
 const addNums = (num1, num2) => {
-  return num1 + num2 + x;
-}
+	return num1 + num2 + x;
+};
 console.log("Question 8 Running: ", addNums(4, 5));
 
 // 9. Write a function that returns another function. (use arrow functions please)
@@ -21,32 +21,34 @@ console.log("Question 9 Running: ", returnFunction()(4, 5));
 
 // 10. given the following code:
 
-
 const getFunction = () => {
-  const y = 5;
+	const y = 5;
 
-  const insideFunc = (a) => y + a;
+	const insideFunc = (a) => y + a;
 
-  return insideFunc;
+	return insideFunc;
 };
 
-console.log(getFunction()(2))
+console.log(getFunction()(2));
 
 // Explain why the function that returns from getFunction still has access to "y" if "y" is not a global variable.
 
-
+//The insideFunc has access to the const y because of closure.
 
 // 11. write a function that takes "couldThrowError()" as a callback argument.
 // within that function call "couldThrowError" and console log its result.
 // Make sure to handle errors that could be thrown by "couldThrowError()"
 
 const couldThrowError = () => {
-  
-  if(Math.ceil(Math.random() * 2) < 2){
-    throw new Error("Error was thrown");
-  }
-  
-  return 'success'
+	if (Math.ceil(Math.random() * 2) < 2) {
+		throw new Error("Error was thrown");
+	}
+
+	return "success";
+};
+
+try {
+  couldThrowError()
+} catch (Error) {
+  console.error(Error)
 }
-
-
